187e5a73c2c930a08b8045427de0a831f86f3537
douglassouzars <douglassouza15.ds@gmail.com>
2022-09-06 11:38:54 -0300

    1 componente

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,13 @@
+import NotificationButton from "./components/NotificationButton"
+
 function App() {
   return(
+    <>
     <h1>Ola teste!</h1>
+    <NotificationButton />
+    <NotificationButton />
+
+    </>
   )
 }
 
