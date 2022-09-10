f9b52c5ebb773026c77ef5ca58e293f45d096826
douglassouzars <douglassouza15.ds@gmail.com>
2022-09-10 15:19:07 -0300

    Security Config

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -7,10 +7,11 @@ function Header() {
         <header>
         <div class="dsmeta-logo-container">
             <img src={logo} alt="DSMeta" />
-            <h1>DSMeta</h1>
+            <h1>Curso de REACT </h1>
+            <h2>FRONTEND FINISHED</h2>
             <p>
               Desenvolvido por
-              <a href="https://www.instagram.com/devsuperior.ig">@devsuperior.ig</a>
+              <a href="https://www.instagram.com/douglassouza_rs"> DOUGLAS SOUZA</a>
             </p>
         </div>
     </header>
